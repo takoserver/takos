00f18687e2288cda12788fc93c7d04558e55de3a
tako0614 <shoutatomiyama0614@gmail.com>
2023-09-22 23:07:49 +0900

    Deno!

--- fresh.gen.ts
+++ fresh.gen.ts
@@ -7,6 +7,7 @@ import * as $1 from "./routes/_app.tsx";
 import * as $2 from "./routes/api/joke.ts";
 import * as $3 from "./routes/greet/[name].tsx";
 import * as $4 from "./routes/index.tsx";
+import * as $5 from "./routes/tako.jsx";
 import * as $$0 from "./islands/Counter.tsx";
 
 const manifest = {
@@ -16,6 +17,7 @@ const manifest = {
     "./routes/api/joke.ts": $2,
     "./routes/greet/[name].tsx": $3,
     "./routes/index.tsx": $4,
+    "./routes/tako.jsx": $5,
   },
   islands: {
     "./islands/Counter.tsx": $$0,
---
+++ routes/tako.jsx
@@ -0,0 +1,13 @@
+// routes/_app.tsx
+export default function tako() {
+  return (
+    <html>
+      <head>
+        <title>takoserver</title>
+      </head>
+      <body>
+        <h1 className="text-xl">takoserver</h1>
+      </body>
+    </html>
+  );
+}
