5f25972a1b3195c1b4b886f71ef35598f8e5fab5
Shotaro Nakamura <79000684+nakasyou@users.noreply.github.com>
2023-10-02 17:05:28 +0900

    Create Header.tsx

---
+++ components/Header.tsx
@@ -0,0 +1,25 @@
+import HeaderMenu from '../islands/HeaderMenu.tsx'
+
+const Contents = () => <ul>
+  <div>ホーム</div>
+  <div>About</div>
+  <div>SNS(開発中)</div>
+</ul>
+
+export default function Header () {
+  return <header>
+    <div className="w-full">
+      <div className="pr-3">
+        <div class="block lg:hidden">
+          <HeaderMenu>
+            <Contents />
+          </HeaderMenu>
+        </div>
+        <a className="logo-area"><img src="/logo.png" alt="takoserver logo" className="logo-img" /></a>
+        <div class="hidden lg:block grid grid-cols-3">
+          <Contents />
+        </div>
+      </div>
+    </div>
+  </header>
+}
