5ef34e83aff770a7996ca8a94fce20b179a34ef3
Shotaro Nakamura <79000684+nakasyou@users.noreply.github.com>
2023-10-02 17:10:30 +0900

    Update index.tsx

--- routes/index.tsx
+++ routes/index.tsx
@@ -1,5 +1,6 @@
 import { useSignal } from "@preact/signals";
 import Counter from "../islands/Counter.tsx";
+import Header from '../components/Header.tsx'
 
 export default function Home() {
   const count = useSignal(3);
@@ -10,31 +11,7 @@ export default function Home() {
         <title>Takoserver project</title>
         <link rel="stylesheet" href="/style.css"></link>
       </head>
-      <header>
-        <div className="header-menu w-full">
-        <div className="header-logo-menu pr-3">
-        <div id="nav-drawer">
-            <input id="nav-input" type="checkbox" className="nav-unshown" />
-            <label id="nav-open" for="nav-input"><span></span></label>
-            <label className="nav-unshown" id="nav-close" for="nav-input"></label>
-            <div id="nav-content">
-              <ul>
-                <li>ホーム</li>
-                <li>About</li>
-                <li>SNS(開発中)</li>
-              </ul>
-            </div>
-        </div>
-        <a className="logo-area"><img src="/logo.png" alt="takoserver logo" className="logo-img" /></a>
-        </div>
-        {/*↓デスクトップ用*/}
-          <ul className="dt-ul-menu">
-            <li className="dt-nav-menu">ホーム</li>
-            <li className="dt-nav-menu">About</li>
-            <li className="dt-nav-menu">SNS(開発中)</li>
-          </ul>
-        </div>
-      </header>
+      <Header />
       <section className="pt-10 pb-10">
         <h1 className="text-5xl text-center text-white">メイン</h1>
         <p className="text-xl text-center text-white">(書いてないだけ)</p>
