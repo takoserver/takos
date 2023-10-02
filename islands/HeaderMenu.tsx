a1769f4f4da988651b2365942ee787deb0a1d35f
Shotaro Nakamura <79000684+nakasyou@users.noreply.github.com>
2023-10-02 17:14:29 +0900

    Update HeaderMenu.tsx

--- islands/HeaderMenu.tsx
+++ islands/HeaderMenu.tsx
@@ -8,12 +8,12 @@ export default function HeaderMenu ({ children }: Props) {
   
   return <>
     <div>
-      <button>🍔</button>
+      <button onClick={() => setIsOpen(true)}>🍔</button>
     </div>
     <div class={`
       fixed top-0 left-0 w-screen h-screen bg-white border p-3
       duration-100
-      ${isOpenMenu ? 'translate-x-0' : 'translate-x-full'}`}
+      ${isOpen ? 'translate-x-0' : 'translate-x-full'}`}
       >
       { children }
     </div>
