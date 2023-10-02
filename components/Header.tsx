29066899513f9b4407119ed1cb05c15d2d0d5996
Shotaro Nakamura <79000684+nakasyou@users.noreply.github.com>
2023-10-02 17:22:28 +0900

    fix

--- components/Header.tsx
+++ components/Header.tsx
@@ -1,10 +1,10 @@
 import HeaderMenu from '../islands/HeaderMenu.tsx'
 
-const Contents = () => <ul>
+const Contents = () => <>
   <div>ホーム</div>
   <div>About</div>
   <div>SNS(開発中)</div>
-</ul>
+</>
 
 export default function Header () {
   return <header>
@@ -15,7 +15,9 @@ export default function Header () {
             <Contents />
           </HeaderMenu>
         </div>
-        <a className="logo-area"><img src="/logo.png" alt="takoserver logo" className="logo-img" /></a>
+        <div>
+          <a className="logo-area"><img src="/logo.png" alt="takoserver logo" className="logo-img" /></a>
+        </div>
         <div class="hidden lg:grid grid-cols-3 text-white">
           <Contents />
         </div>
