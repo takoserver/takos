838b61127b831e11dc4e208079d3a269776f2a98
tako0614 <shoutatomiyama0614@gmail.com>
2023-10-02 16:17:43 +0900

    メインページ

--- routes/index.tsx
+++ routes/index.tsx
@@ -4,22 +4,46 @@ import Counter from "../islands/Counter.tsx";
 export default function Home() {
   const count = useSignal(3);
   return (
-    <div class="px-4 py-8 mx-auto bg-[#86efac]">
-      <div class="max-w-screen-md mx-auto flex flex-col items-center justify-center">
-        <img
-          class="my-6"
-          src="/logo.svg"
-          width="128"
-          height="128"
-          alt="the Fresh logo: a sliced lemon dripping with juice"
-        />
-        <h1 class="text-4xl font-bold">Welcome to Fresh</h1>
-        <p class="my-4">
-          Try updating this message in the
-          <code class="mx-2">./routes/index.tsx</code> file, and refresh.
-        </p>
-        <Counter count={count} />
-      </div>
-    </div>
+    <>
+    <html>
+      <head>
+        <title>Takoserver project</title>
+        <link rel="stylesheet" href="/style.css"></link>
+      </head>
+      <header>
+        <div className="header-menu w-full">
+        <div className="header-logo-menu pr-3">
+        <div id="nav-drawer">
+            <input id="nav-input" type="checkbox" className="nav-unshown" />
+            <label id="nav-open" for="nav-input"><span></span></label>
+            <label className="nav-unshown" id="nav-close" for="nav-input"></label>
+            <div id="nav-content">
+              <ul>
+                <li>ホーム</li>
+                <li>About</li>
+                <li>SNS(開発中)</li>
+              </ul>
+            </div>
+        </div>
+        <a className="logo-area"><img src="/logo.png" alt="takoserver logo" className="logo-img" /></a>
+        </div>
+        {/*↓デスクトップ用*/}
+          <ul className="dt-ul-menu">
+            <li className="dt-nav-menu">ホーム</li>
+            <li className="dt-nav-menu">About</li>
+            <li className="dt-nav-menu">SNS(開発中)</li>
+          </ul>
+        </div>
+      </header>
+      <section className="pt-10 pb-10">
+        <h1 className="text-5xl text-center text-white">メイン</h1>
+        <p className="text-xl text-center text-white">(書いてないだけ)</p>
+      </section>
+      <footer>
+        <p className="text-white text-center">Copyright © 2021-2023 Takoserver All Rights Reserved.</p>
+        <p className="text-white text-center"><a href="https://aranpect.com">Aranpect</a></p>
+      </footer>
+    </html>
+    </>
   );
 }
