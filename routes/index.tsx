48ca4a11e1eae2f447f0ed2318f4a5e8007178f9
Shota Tomiyama <shoutatomiyama0614@gmail.com>
2023-11-16 16:53:31 +0900

    Technology for All Knowledge Openness

--- routes/index.tsx
+++ routes/index.tsx
@@ -13,16 +13,25 @@ export default function Home() {
         <div class="full">
           <div  class="full-title">
           <h1>takoserver project</h1>
-          <p class="text-lg"> tako: Technology for All Knowledge Openness</p>
+          <p class="text-lg">Technology for All Knowledge Openness</p>
           </div>
         </div>
       <section class="pt-16 pb-10">
         <h1 class="text-5xl text-center text-white">takoserver</h1>
-        <p class="text-xl text-center text-white">(書いてないだけ)</p>
-        <div class="m-auto w-3/4 text-white">
+        <p class="text-xl text-center text-white">takoserverは</p>
+        <div class="m-auto w-3/4 text-white md:flex">
+          <div class="md:w-1/2">
+          <img class="m-full" src="logo-mine.jpg" alt="キャラクター" />
+          <p class="text-l text-center text-white">takoserver公式マスコット</p>
+          </div>
+          <div class="md:w-1/2 md:ml-8">
           <p>
-            takoserver
+          ※TAKOserver NEXUSとは無関係です<br />
+          ※takoserverにもマインクラフトサーバーはありましたが現在は停止中です<br />
+          takoserverは(LINE+twitter+instagram)÷4みたいな分散型SNSを作ることを目指しています<br />
+          最近親にルーターを侵略されたせいでvpn経由で公開しております<br />
           </p>
+          </div>
         </div>
       </section>
       <Footer />
