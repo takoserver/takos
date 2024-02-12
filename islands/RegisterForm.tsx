9914f51e43553d83978c65967a236973c2e3f0ca
Tomiyama Shota <shoutatomiyama0614@gmail.com>
2024-02-12 13:21:30 +0900

    header修正 アニメーション追加

--- islands/RegisterForm.tsx
+++ islands/RegisterForm.tsx
@@ -1,6 +1,5 @@
 import { useState } from 'preact/hooks';
 
-
 export default function RegisterForm({ text, color,tako }: { text: string, color: string; tako: string;}) {
     const classs = "inline-flex items-center justify-center rounded-md text-sm font-medium ring-offset-background transition-colors focus-visible:outline-none focus-visible:ring-2 focus-visible:ring-ring focus-visible:ring-offset-2 disabled:pointer-events-none disabled:opacity-50" + color 
 
@@ -16,10 +15,10 @@ return <>
         {text}
     </button>
     {showModal && (
-        <div className="modal">
+        <div className="modal fade-in">
           <div className="modal-content">
             <span className="close" onClick={handleButtonClick}>×</span>
-            <p>ここにフォームを追加します</p>
+            <p class="	text-black">text message</p>
           </div>
         </div>
       )}
