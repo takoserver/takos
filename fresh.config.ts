4c00a6825962359f5dc8cd1e87155bb466fe15e7
Tomiyama Shota <shoutatomiyama0614@gmail.com>
2024-02-14 16:22:32 +0900

    update tailwind css

--- fresh.config.ts
+++ fresh.config.ts
@@ -1,6 +1,7 @@
 import { defineConfig } from "$fresh/server.ts";
-import twindPlugin from "$fresh/plugins/twind.ts"
-import twindConfig from "./twind.config.ts";
+//import twindPlugin from "$fresh/plugins/twind.ts"
+//import twindConfig from "./twind.config.ts";
+import tailwind from "$fresh/plugins/tailwind.ts";
 export default defineConfig({
-  plugins: [twindPlugin(twindConfig)]
+  plugins: [tailwind()],
 });
