473bc2a685603478e2d20b884e33ac55d6f0f1c0
Tomiyama Shota <shoutatomiyama0614@gmail.com>
2024-03-11 22:21:37 +0900

    register csrf token 削除

--- routes/api/logins/talk.js
+++ routes/api/logins/talk.js
@@ -1,7 +1,25 @@
-export const tali = () => {
-    const { response, socket } = Deno.upgradeWebSocket(c.req);
-
-    // websocketのハンドリング
-    socket.addEventListener("message", (e) => console.log(e));
-    return response;
-}
+import { isCsrftoken } from "../../../util/takoFunction.ts";
+export const handler = {
+    GET(req) {
+        if (req.headers.get("upgrade") === "websocket") {
+        const { socket, response } = Deno.upgradeWebSocket(req);
+        if (!socket) throw new Error("unreachable");
+        /*if(!isCsrftoken(req.body.token)) {
+            return new Response({})
+        }*/
+        //アカウント認証
+        socket.onmessage = (ev) => {
+            socket.send(ev.data);
+        };
+        //socket.onclose = () => {
+        //socket.send("Hello from the client!");
+        //};
+        socket.onopen = () => {
+        socket.send("Hello from the client!");
+        };
+        return response;
+        } else {
+            return new Response(JSON.stringify({response: "the request is a normal HTTP request"}));
+        }
+    },
+  };
