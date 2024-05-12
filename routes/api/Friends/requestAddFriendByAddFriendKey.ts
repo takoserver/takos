436818bc1c8daed3d7873f8cca3650718f33ba26
tako0614 <shoutatomiyama0614@gmail.com>
2024-05-12 11:45:57 +0900

    ts!

--- routes/api/Friends/requestAddFriendByAddFriendKey.ts
+++ routes/api/Friends/requestAddFriendByAddFriendKey.ts
@@ -1,7 +1,7 @@
 import { getCookies } from "https://deno.land/std@0.220.1/http/cookie.ts"
 import csrftoken from "../../../models/csrftoken.ts"
 import Friends from "../../../models/friends.ts"
-import requestAddFriend from "../../../models/reqestAddFriend.js"
+import requestAddFriend from "../../../models/reqestAddFriend.ts"
 import Users from "../../../models/users.ts";
 export const handler = {
   async POST(req: Request,ctx: any) {
