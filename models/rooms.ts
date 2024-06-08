56f2895a8a4359374828211eb30ee25c41ce5f86
tako0614 <shoutatomiyama0614@gmail.com>
2024-06-09 07:02:59 +0900

    feat: Update room name validation in rooms.ts

--- models/rooms.ts
+++ models/rooms.ts
@@ -3,13 +3,6 @@ export const roomsSchema = new mongoose.Schema({
   uuid: {
     type: String,
     required: true,
-    validate: {
-      validator: function (v: string) {
-        return /^[a-zA-Z0-9-_]{4,16}$/.test(v)
-      },
-      message: (props: { value: any }) =>
-        `${props.value} is not a valid room name!`,
-    },
   },
   showName: {
     type: String,
