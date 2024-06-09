959e3d3afcd8aa264c50438bdd33772e31c4e72c
tako0614 <shoutatomiyama0614@gmail.com>
2024-06-09 19:00:34 +0900

    Refactor friend request handler to handle friend creation and update in a more efficient way

--- routes/api/v1/friends/request.ts
+++ routes/api/v1/friends/request.ts
@@ -218,14 +218,12 @@ export const handler = {
       if (!isAlreadyCreateFriendTable) {
         await Friends.create({ user: userid })
       }
-      await Friends.updateOne(
-        { user: userid },
-        { $push: { friends: { userid: friendInfo.uuid } } }, // Assuming 'name' is a valid field in the 'friends' object
-      )
-      await Friends.updateOne(
-        { user: friendInfo.uuid },
-        { $push: { friends: { userid } } },
-      )
+      if((await Friends.updateOne({ user: userid },{ $push: { friends: { userid: friendInfo.uuid } } },)).matchedCount === 0){
+        await Friends.create({ user: userid, friends: [{ userid: friendInfo.uuid }] })
+      }
+      if((await Friends.updateOne({ user: friendInfo.uuid },{ $push: { friends: { userid } } },)).matchedCount === 0){
+        await Friends.create({ user: friendInfo.uuid, friends: [{ userid }] })
+      }
       await requestAddFriend.updateOne(
         { userID: userid },
         { $pull: { Applicant: { userID: friendInfo.uuid } } },
