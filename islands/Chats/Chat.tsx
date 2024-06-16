869a192ee20019469e419037c1a02235d507a5b5
tako0614 <shoutatomiyama0614@gmail.com>
2024-06-16 18:35:41 +0900

    chore: Fix typo in Chat.tsx file

--- islands/Chats/Chat.tsx
+++ islands/Chats/Chat.tsx
@@ -275,8 +275,7 @@ export default function Home(
                                         class="p-talk-chat-send__file"
                                         onClick={() => {
                                             if (Message) {
-                                                if (messages.length > 100) {
-                                                    
+                                                if (Message.length > 100) {
                                                     return
                                                 }
                                                 const data = {
