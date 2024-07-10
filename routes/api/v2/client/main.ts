acfd7c486f70be99c3db0e0e45bf23b50c1b976a
takoserver <96359093+tako0614@users.noreply.github.com>
2024-07-10 13:13:13 +0900

    create websocket api

--- routes/api/v2/client/main.ts
+++ routes/api/v2/client/main.ts
@@ -1,1 +1,12 @@
 //websocket
+export const handler = {
+    GET(req: Request,ctx: any) {
+        if (!ctx.state.data.loggedIn) {
+            return new Response(JSON.stringify({ status: "Please Login" }), {
+              headers: { "Content-Type": "application/json" },
+              status: 401,
+            })
+        }
+        //
+    }
+}
