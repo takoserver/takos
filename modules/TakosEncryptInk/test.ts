59fd69cec95febd1d92fdb950bf8269ef726af0b
tako0614 <shoutatomiyama0614@gmail.com>
2024-08-21 15:22:03 +0900

    feat: Add lazy loading for images and encryptDataDeviceKey function

--- modules/TakosEncryptInk/test.ts
+++ modules/TakosEncryptInk/test.ts
@@ -17,44 +17,5 @@ const master = await createMasterKey();
 const keys = await createIdentityKeyAndAccountKey(master);
 const identityKey = keys.identityKey;
 const accountKey = keys.accountKey;
-const deviceKey = await createDeviceKey(master);
 const roomKey = await createRoomKey(identityKey);
-/*
-let data: string = "";
-for (let i = 0; i < 10000; i++) {
-  data += "test"
-}
-
-// test device key encryption
-const encryptedData = await encryptDataDeviceKey(deviceKey, data);
-const decryptedData = await decryptDataDeviceKey(deviceKey, encryptedData);
-console.log(decryptedData);
-
-// test room key encryption
-const encryptedDataRoomKey = await encryptAndSignDataWithRoomKey(roomKey, data, identityKey);
-const decryptedDataRoomKey = await decryptAndVerifyDataWithRoomKey(roomKey, encryptedDataRoomKey, identityKey.public);
-console.log(decryptedDataRoomKey);
-
-// test account key encryption
-const masterKey2 = await createMasterKey();
-const keys2 = await createIdentityKeyAndAccountKey(masterKey2);
-const identityKey2 = keys2.identityKey;
-const accountKey2 = keys2.accountKey;
-const encryptedDataAccountKey = await encryptAndSignDataWithAccountKey(accountKey2.public, data, identityKey)
-const decryptedDataAccountKey = await decryptAndVerifyDataWithAccountKey(accountKey2, encryptedDataAccountKey, identityKey.public);
-console.log(decryptedDataAccountKey);*/
-//時間計測開始
-const startTime = performance.now();
-const image = await Deno.readFile("./testImage.webp");
-const base64 = arrayBufferToBase64(image);
-const encryptedData = await encryptDataDeviceKey(deviceKey, base64);
-const decryptedData = await decryptDataDeviceKey(deviceKey, encryptedData);
-if(decryptedData) {
-    const image = base64ToArrayBuffer(decryptedData);
-    await Deno.writeFile("./testImage2.webp", new Uint8Array(image));
-    const endTime = performance.now();
-    //秒に変換
-    const time = (endTime - startTime) / 1000;
-    console.log(time + "秒");
-}
 
