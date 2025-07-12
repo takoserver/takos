ce0b031ef2a7bd3fded0e052372924cbf9c702c3
tako0614 <shoutatomiyama0614@gmail.com>
2025-07-13 03:33:10 +0900

    chore: add vite-plugin-pwa as a dev dependency in package.json

--- app/client/src/main.tsx
+++ app/client/src/main.tsx
@@ -1,9 +1,9 @@
 /* @refresh reload */
+
+// <reference types="virtual:pwa-register" />
+
 import { render } from "solid-js/web";
-import { registerSW } from "virtual:pwa-register";
 
 import App from "./App.tsx";
 
 render(() => <App />, document.getElementById("root")!);
-// サービスワーカーを登録してPWAを有効化
-registerSW({ immediate: true });
